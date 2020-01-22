bb782e76b2eaf69be53017fd7e2f44901f4ea97c
Nina Potrebich and Vera Weikel <vera@>
2020-01-22 15:29:27 -0800

    Updates to Ingredients:Controller, Views for UD of crud.

--- RecipeBox/Controllers/IngredientsController.cs
+++ RecipeBox/Controllers/IngredientsController.cs
@@ -47,65 +47,37 @@ namespace RecipeBox.Controllers
       return View(thisIngredient);
     }
 
-//     public ActionResult Edit(int id)
-//     {
-//       var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId == id);
-//       ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
-//       return View(thisIngredient);
-//     }
+    public ActionResult Edit(int id)
+    {
+      var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId == id);
+      return View(thisIngredient);
+    }
 
-//     [HttpPost]
-//     public ActionResult Edit(Ingredient ingredient, int RecipeId)
-//     {
-//       if (RecipeId != 0)
-//       {
-//         _db.RecipeIngredient.Add(new RecipeIngredient() { RecipeId = RecipeId, IngredientId = ingredient.IngredientId });
-//       }
-//       _db.Entry(ingredient).State = EntityState.Modified;
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Edit(Ingredient ingredient)
+    {
+      if (_db.Ingredients.FirstOrDefault(ing => ing.IngredientDescription == ingredient.IngredientDescription) ==null)
+      {
+        _db.Entry(ingredient).State = EntityState.Modified;
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
 
-//     public ActionResult AddRecipe(int id)
-//     {
-//       var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId == id);
-//       ViewBag.RecipeId = new SelectList(_db.Recipes, "RecipeId", "Name");
-//       return View(thisIngredient);
-//     }
+    public ActionResult Delete(int id)
+    {
+      var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId == id);
+      return View(thisIngredient);
+    }
 
-//     [HttpPost]
-//     public ActionResult AddRecipe(Ingredient ingredient, int RecipeId)
-//     {
-//       if (RecipeId != 0)
-//       {
-//         _db.RecipeIngredient.Add(new RecipeIngredient() { RecipeId = RecipeId, IngredientId = ingredient.IngredientId });
-//       }
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId == id);
+      _db.Ingredients.Remove(thisIngredient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//     public ActionResult Delete(int id)
-//     {
-//       var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId == id);
-//       return View(thisIngredient);
-//     }
-
-//     [HttpPost, ActionName("Delete")]
-//     public ActionResult DeleteConfirmed(int id)
-//     {
-//       var thisIngredient = _db.Ingredients.FirstOrDefault(ingredients => ingredients.IngredientId == id);
-//       _db.Ingredients.Remove(thisIngredient);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-
-//     [HttpPost]
-//     public ActionResult DeleteRecipe(int joinId)
-//     {
-//       var joinEntry = _db.RecipeIngredient.FirstOrDefault(entry => entry.RecipeIngredientId == joinId);
-//       _db.RecipeIngredient.Remove(joinEntry);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
   }
 }
